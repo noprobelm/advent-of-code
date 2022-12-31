4a86354a44777ce424cce7897224d00b98458263
noprobelm <noprobelm@protonmail.com>
2022-12-31 15:27:50 -0500

    Added docstrings

--- 2022/day-5.py
+++ 2022/day-5.py
@@ -3,12 +3,39 @@ import numpy as np
 
 
 class Crates:
+    """
+    Container for Crates and their positions based on the text input from 'day-5-input.txt', which is the puzzle input
+    for day 5 of the 2022 'Advent of Code' series. Crates can be moved using the 'move' method, which references the
+    procedure embedded in the puzzle input.
+
+    Attributes
+    -------
+    layout : dict[int, list[str]]
+        The layout of crates as they're currently stacked. Keys are the stack number, values are the bottom-to-top
+        order of the crates.
+    procedure : list[dict[str, int]]
+        The iterative procedure the crane will follow. Keys for each dict are 'to', 'from', and 'qty'
+    message : str
+        The secret message revealed after all crates have been moved. This is the puzzle's answer.
+
+    Methods
+    -------
+    reset()
+        Resets the crates to their original positions.
+    format_procedure(procedure: list[str])
+        Static method: Used to translate the list of str instructions into a list of dicts structure
+    format_layout(layout: list[str])
+        Static method: Used to translate the unformatted layout of the crates into a dict structure
+    move(model: str)
+        Executes the procedure. 'Model' is the crane model to use for the method's logic (i.e. '9000' or '9001')
+    """
     def __init__(self):
         self.layout = None
         self.procedure = None
         self.reset()
 
     def reset(self):
+        """Resets the crates to their original positions"""
         with open("day-5-input.txt", "r") as f:
             self.layout = []
             self.procedure = []
@@ -40,6 +67,18 @@ class Crates:
 
     @staticmethod
     def format_procedure(procedure):
+        """Used to translate the list of str instructions into a list of dicts structure
+
+        Parameters
+        -------
+        procedure : list[str]
+            List of the str-type instructions for the crane to carry out.
+
+        Returns
+        -------
+        procedure_formatted : list[dict[str, int]]
+            List of dicts of discrete categories to their respective int values
+        """
         procedure_formatted = []
         for line in procedure:
             matches = [int(match) for match in re.findall(r"\d+", line)]
@@ -49,11 +88,20 @@ class Crates:
 
     @staticmethod
     def format_layout(layout):
-        layout_formatted = {}
+        """Used to translate the unformatted layout of the crates into a dict structure
 
+        Parameters
+        -------
+        layout : list[str]
+            List of strings with the original crate position layout embedded.
+
+        Returns
+        -------
+        layout_formatted : dict[int, list[str]]
+            dict of crate stack numbers to the bottom-to-top ordering
+        """
+        layout_formatted = {}
         for idx, line in enumerate(layout):
-            line = re.sub("\n", " ", line)
-            line = re.sub(r"[\[\]]", r" ", line)
             line = line.ljust(max([len(line) for line in layout]))
             line = list(line)
             layout[idx] = line
@@ -67,17 +115,18 @@ class Crates:
             if re.match(r"\d", char):
                 stack = int(char)
                 layout_formatted[stack] = []
-            else:
+            elif re.match(r"\w", char):
                 layout_formatted[stack].append(char)
 
         return layout_formatted
 
 
-crates = Crates()
-crates.move(model="9000")
-print(f"The hidden message with CrateMover 9000 is {crates.message}")
-crates.reset()
+if __name__ == '__main__':
+    crates = Crates()
+    crates.move(model="9000")
+    print(f"The hidden message with CrateMover 9000 is {crates.message}")
+    crates.reset()
 
-crates = Crates()
-crates.move(model="9001")
-print(f"The hidden message with CrateMover 9001 is {crates.message}")
+    crates = Crates()
+    crates.move(model="9001")
+    print(f"The hidden message with CrateMover 9001 is {crates.message}")
