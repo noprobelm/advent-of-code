0dd4bf7032287e4589c1f6a447721cb0dc650ae0
noprobelm <noprobelm@protonmail.com>
2023-01-08 10:58:56 -0500

    Completed part 1 of puzzle

--- 2022/day_8.py
+++ 2022/day_8.py
@@ -1,54 +1,74 @@
-from typing import List, Optional, Sequence
+import numpy as np
 from rich.console import Console
 from dataclasses import dataclass
-import networkx as nx
 console = Console()
-
-
-@dataclass(frozen=True, order=True)
-class Tree:
-    x: int
-    y: int
-
-class Forest:
-    def __init__(self, data: List[List[int]]):
-        self.forest = nx.MultiDiGraph()
-        cardinals = ((0, 1), (1, 0), (0, -1), (-1, 0))
-        for y in range(len(data)):
-            for x in range(len(data[y])):
-                tree = Tree(x=x, y=y)
-                tree_height = data[y][x]
-                self.forest.add_node(tree, tree_height=tree_height)
-                for cardinal in cardinals:
-                    try:
-                        neighbor_x = tree.x + cardinal[0]
-                        neighbor_y = tree.y + cardinal[1]
-                        if neighbor_x < 0 or neighbor_y < 0:
-                            continue
-                        neighbor_height = data[neighbor_y][neighbor_x]
-                        neighbor = Tree(x=neighbor_x, y=neighbor_y)
-                        if tree_height > neighbor_height:
-                            is_taller = True
-                        else:
-                            is_taller = False
-
-                        self.forest.add_edge(tree, neighbor, is_taller=is_taller)
-                    except IndexError:
-                        continue
 
 
 @dataclass
 class Tree:
-    visible: dict
+    x: int
+    y: int
+    height: int = 0
+    is_visible: bool = None
+    visibility_score: int = 0
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __eq__(self, other):
+        if other.x == self.x and other.y == self.y:
+            return True
 
 
-class Forest
+class Forest:
     def __init__(self, data):
+        self.input_data = data
         self.forest = {}
         for y in range(len(data)):
             for x in range(len(data[y])):
-                self.forest[x, y] = data[y][x]
-    print(data)
+                self.forest[Tree(x, y, height=0)] = {}
+
+    @property
+    def num_visible(self):
+        return sum([1 for tree in self.forest if tree.is_visible])
+
+    @staticmethod
+    def get_row_visibility(row):
+        trees = [True]
+        tallest = row[0]
+        for tree in row[1:]:
+            if tree <= tallest:
+                visible = False
+            else:
+                visible = True
+                tallest = tree
+            trees.append(visible)
+        return trees
+
+    def run(self):
+        directions = {'west': 1, 'east': -1, 'north': 1, 'south': -1}
+        data = np.array(self.input_data)
+        for y in range(len(data)):
+            for direction in ['west', 'east']:
+                visibility = self.get_row_visibility(data[y][::directions[direction]])
+                visibility = visibility[::directions[direction]]
+                for x in range(len(data[y])):
+                    self.forest[Tree(x, y)][direction] = visibility[x]
+        data = np.transpose(data)
+        for x in range(len(data)):
+            for direction in ['north', 'south']:
+                visibility = self.get_row_visibility(data[x][::directions[direction]])
+                visibility = visibility[::directions[direction]]
+                for y in range(len(data[x])):
+                    self.forest[Tree(x, y)][direction] = visibility[y]
+
+        for tree in self.forest:
+            for direction in self.forest[tree].copy():
+                if self.forest[tree][direction] is True:
+                    tree.is_visible = True
+                    break
+                else:
+                    tree.is_visible = False
 
 
 if __name__ == '__main__':
@@ -56,3 +76,7 @@ if __name__ == '__main__':
         data = [[int(height) for height in line.strip()] for line in f.readlines()]
 
     forest = Forest(data)
+    forest.run()
+    arr = np.array(forest.num_visible)
+    visible = forest.num_visible
+    print(visible)
