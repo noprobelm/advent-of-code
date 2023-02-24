ecfe25aae267ccfc75617a73da6a0ab69b3da97f
noprobelm <noprobelm@protonmail.com>
2023-02-24 10:51:37 -0500

    Removed unused import, added type hinting

--- 2022/day_12.py
+++ 2022/day_12.py
@@ -1,7 +1,6 @@
 import networkx as nx
 from dataclasses import dataclass
 from rich import print
-from rich.console import Console
 
 
 @dataclass(frozen=True)
@@ -11,7 +10,7 @@ class Node:
 
 
 class Maze:
-    def __init__(self, mapper):
+    def __init__(self, mapper: list[list[str]]) -> None:
         self.mapper = mapper
         self.paths = nx.DiGraph()
         for y, row in enumerate(self.mapper):
@@ -28,7 +27,7 @@ class Maze:
         for node in self.paths.nodes:
             self.add_neighbors(node)
 
-    def add_neighbors(self, node):
+    def add_neighbors(self, node: Node) -> None:
         if node.x > 0:
             neighbor = Node(node.x - 1, node.y)
             self.paths.add_node(
@@ -109,11 +108,11 @@ class Maze:
                 self.paths.add_edge(node, neighbor)
 
     @property
-    def shortest(self):
+    def shortest(self) -> int:
         return len(nx.shortest_path(self.paths, self.start, self.end)) - 1
 
     @property
-    def shortest_from_a(self):
+    def shortest_from_a(self) -> int:
         shortest = []
         for node in self.paths.nodes:
             if self.paths.nodes[node]["altitude"] == ord("a"):
@@ -124,22 +123,21 @@ class Maze:
         return min(shortest) - 1
 
     @property
-    def start(self):
+    def start(self) -> Node:
         return list(nx.get_node_attributes(self.paths, "start").keys())[0]
 
     @property
-    def end(self):
+    def end(self) -> Node:
         return list(nx.get_node_attributes(self.paths, "end").keys())[0]
 
 
-def read(filename):
+def read(filename: str) -> list[list[str]]:
     with open(filename, "r") as f:
         mapper = [list(row.strip()) for row in f.readlines()]
     return mapper
 
 
 if __name__ == "__main__":
-    console = Console()
     maze = Maze(read("day-12-input.txt"))
     print(maze.shortest)
     print(maze.shortest_from_a)
