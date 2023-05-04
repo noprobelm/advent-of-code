f3ae5731c8590e6d1942ce3474c1ae7180b050f0
noprobelm <noprobelm@protonmail.com>
2023-05-04 15:59:57 -0400

    Cleaned up code

--- 2022/day_13.py
+++ 2022/day_13.py
@@ -1,5 +1,3 @@
-import re
-
 SAMPLE = """
 [1,1,3,1,1]
 [1,1,5,1,1]
@@ -26,90 +24,92 @@ SAMPLE = """
 [1,[2,[3,[4,[5,6,0]]]],8,9]"""
 
 
+class Packet:
+    def __init__(self, data: list):
+        self.data = data
+
+    def __str__(self):
+        return str(self.data)
+
+    def __repr__(self):
+        return str(self.data)
+
+    def __lt__(self, other: "Packet"):
+        if not isinstance(other, Packet):
+            raise TypeError("Incomparable types")
+        for i in range(max([len(self.data), len(other.data)])):
+            if i + 1 > len(self.data):
+                return True
+            elif i + 1 > len(other.data):
+                return False
+            elif isinstance(self.data[i], list) and isinstance(other.data[i], list):
+                if self.data[i] == other.data[i]:
+                    continue
+                return Packet(self.data[i]) < Packet(other.data[i])
+            elif isinstance(self.data[i], int) and isinstance(other.data[i], int):
+                if self.data[i] == other.data[i]:
+                    continue
+                return self.data[i] < other.data[i]
+            elif isinstance(self.data[i], int) and isinstance(other.data[i], list):
+                if [self.data[i]] == other.data[i]:
+                    continue
+                return Packet([self.data[i]]) < Packet(other.data[i])
+
+            elif isinstance(other.data[i], int) and isinstance(self.data[i], list):
+                if self.data[i] == [other.data[i]]:
+                    continue
+                return Packet(self.data[i]) < Packet([other.data[i]])
+
+
 def get_packets(input_str: str) -> list:
-    puzzle_parsed = []
     packets = []
     input_str = input_str.split("\n")
     for line in input_str:
         if line.startswith("["):
-            packets.append(eval(f"list({line})"))
+            packet = eval(f"list({line})")
+            packet = Packet(packet)
+            packets.append(packet)
 
     return packets
 
 
-def get_pairs(packets):
+def solve_puzzle_1(packets):
     pairs = []
     for num, packet in enumerate(packets):
         num += 1
         if num % 2 == 1:
             pairs.append([packet, packets[num]])
 
-    return pairs
+    ordered_idx = []
+    for pair in pairs:
+        ordered_idx.append(pair[0] < pair[1])
+    print(
+        f"The sum of the index of each correctly-ordered packet pair is {sum([i + 1 for i in range(len(ordered_idx)) if ordered_idx[i] is True])}",
+        end="\n\n",
+    )
 
 
-def compare_ints(left, right):
-    if left < right:
-        return True
-    elif left > right:
-        return False
-    else:
-        return None
+def solve_puzzle_2(packets):
+    first_divider = Packet([[2]])
+    second_divider = Packet([[6]])
+    packets.extend([first_divider, second_divider])
+    packets = list(sorted(packets))
 
+    product = 1
+    for num, packet in enumerate(packets):
+        if packet.data == first_divider.data or packet.data == second_divider.data:
+            print(f"Divider {packet} is at idx {num + 1}")
+            product *= num + 1
 
-def is_ordered(left, right):
-    for i in range(max([len(left), len(right)])):
-        if i + 1 > len(left):
-            return True
-        elif i + 1 > len(right):
-            return False
-        elif isinstance(left[i], list) and isinstance(right[i], list):
-            ordered = is_ordered(left[i], right[i])
-            if ordered is not None:
-                return ordered
-        elif isinstance(left[i], int) and isinstance(right[i], int):
-            ordered = compare_ints(left[i], right[i])
-            if ordered is not None:
-                return ordered
-        elif isinstance(left[i], int) and isinstance(right[i], list):
-            ordered = is_ordered([left[i]], right[i])
-            if ordered is not None:
-                return ordered
-
-        elif isinstance(right[i], int) and isinstance(left[i], list):
-            ordered = is_ordered(left[i], [right[i]])
-            if ordered is not None:
-                return ordered
-
-
-def bubble_sort(packets):
-    for i in range(len(packets)):
-        already_sorted = True
-        for j in range(len(packets) - i - 1):
-            if is_ordered(packets[j], packets[j + 1]) is False:
-                packets[j], packets[j + 1] = packets[j + 1], packets[j]
-                already_sorted = False
-
-        if already_sorted:
-            break
-    return packets
+    print(f"The product of each packet divider's index is {product}")
 
 
 if __name__ == "__main__":
     with open("day-13-input.txt", "r") as f:
         input_str = f.read()
-    packets = get_packets(input_str)
-    pairs = get_pairs(packets)
-    ordered_idx = []
-    for num, pair in enumerate(pairs):
-        ordered_idx.append(is_ordered(pair[0], pair[1]))
-    print(sum([i + 1 for i in range(len(ordered_idx)) if ordered_idx[i] is True]))
-    packets.append([[2]])
-    packets.append([[6]])
 
-    packets = bubble_sort(packets)
-    divider_idx = [
-        packets.index(packet) + 1
-        for packet in packets
-        if packet == [[6]] or packet == [[2]]
-    ]
-    print(divider_idx[0] * divider_idx[1])
+    packets = get_packets(input_str)
+    print("PUZZLE 1")
+    solve_puzzle_1(packets)
+    print("PUZZLE 2")
+    solve_puzzle_2(packets)
