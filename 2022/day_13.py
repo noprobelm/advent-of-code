7a60089dfc403834893b521b874976b6be9d5d34
noprobelm <noprobelm@protonmail.com>
2023-05-04 14:15:12 -0400

    Solved puzzle 2

--- 2022/day_13.py
+++ 2022/day_13.py
@@ -32,14 +32,19 @@ def get_packets(input_str: str) -> list:
     input_str = input_str.split("\n")
     for line in input_str:
         if line.startswith("["):
-            puzzle_parsed.append(eval(f"list({line})"))
-
-    for num, packet in enumerate(puzzle_parsed):
-        num += 1
-        if num % 2 == 1:
-            packets.append([packet, puzzle_parsed[num]])
+            packets.append(eval(f"list({line})"))
 
     return packets
+
+
+def get_pairs(packets):
+    pairs = []
+    for num, packet in enumerate(packets):
+        num += 1
+        if num % 2 == 1:
+            pairs.append([packet, packets[num]])
+
+    return pairs
 
 
 def compare_ints(left, right):
@@ -51,14 +56,14 @@ def compare_ints(left, right):
         return None
 
 
-def compare_lists(left, right):
+def is_ordered(left, right):
     for i in range(max([len(left), len(right)])):
         if i + 1 > len(left):
             return True
         elif i + 1 > len(right):
             return False
         elif isinstance(left[i], list) and isinstance(right[i], list):
-            ordered = compare_lists(left[i], right[i])
+            ordered = is_ordered(left[i], right[i])
             if ordered is not None:
                 return ordered
         elif isinstance(left[i], int) and isinstance(right[i], int):
@@ -66,28 +71,45 @@ def compare_lists(left, right):
             if ordered is not None:
                 return ordered
         elif isinstance(left[i], int) and isinstance(right[i], list):
-            ordered = compare_lists([left[i]], right[i])
+            ordered = is_ordered([left[i]], right[i])
             if ordered is not None:
                 return ordered
 
         elif isinstance(right[i], int) and isinstance(left[i], list):
-            ordered = compare_lists(left[i], [right[i]])
+            ordered = is_ordered(left[i], [right[i]])
             if ordered is not None:
                 return ordered
 
 
-def compare_different(left, right):
-    pass
+def bubble_sort(packets):
+    for i, packet in enumerate(packets):
+        already_sorted = True
+        for j in range(len(packets) - i - 1):
+            if is_ordered(packets[j], packets[j + 1]) is False:
+                packets[j], packets[j + 1] = packets[j + 1], packets[j]
+                already_sorted = False
+
+        if already_sorted:
+            break
+    return packets
 
 
 if __name__ == "__main__":
     with open("day-13-input.txt", "r") as f:
         input_str = f.read()
     packets = get_packets(input_str)
+    pairs = get_pairs(packets)
     ordered_idx = []
-    for num, pair in enumerate(packets):
-        ordered = []
-        left = pair[0]
-        right = pair[1]
-        ordered_idx.append(compare_lists(left, right))
+    for num, pair in enumerate(pairs):
+        ordered_idx.append(is_ordered(pair[0], pair[1]))
     print(sum([i + 1 for i in range(len(ordered_idx)) if ordered_idx[i] is True]))
+    packets.append([[2]])
+    packets.append([[6]])
+
+    packets = bubble_sort(packets)
+    divider_idx = [
+        packets.index(packet) + 1
+        for packet in packets
+        if packet == [[6]] or packet == [[2]]
+    ]
+    print(divider_idx[0] * divider_idx[1])
