28b1d6ca82e95bfa160fbdb781b9c76b7f43b176
noprobelm <noprobelm@protonmail.com>
2023-05-19 16:57:21 -0400

    Removed unused files

---
+++ 2022/rust/day_1/target/debug/day_1.d
@@ -0,0 +1,1 @@
+/home/noprobelm/workshop/released/advent-of-code/2022/rust/day_1/target/debug/day_1: /home/noprobelm/workshop/released/advent-of-code/2022/rust/day_1/src/main.rs
---
+++ 2022/rust/day_1/target/debug/deps/day_1-ee6d274a20be5f6a.d
@@ -0,0 +1,5 @@
+/home/noprobelm/workshop/released/advent-of-code/2022/rust/day_1/target/debug/deps/day_1-ee6d274a20be5f6a: src/main.rs
+
+/home/noprobelm/workshop/released/advent-of-code/2022/rust/day_1/target/debug/deps/day_1-ee6d274a20be5f6a.d: src/main.rs
+
+src/main.rs:
