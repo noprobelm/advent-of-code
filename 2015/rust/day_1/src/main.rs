5449ac0bbad599216271a861113d3d7f7a051383
noprobelm <noprobelm@protonmail.com>
2023-11-20 11:39:04 -0500

    Added boilerplate documentation for day 1

--- 2015/rust/day_1/src/main.rs
+++ 2015/rust/day_1/src/main.rs
@@ -1,6 +1,36 @@
+//! # Advent of Code - 2015 Day 1:
+//!
+//! Santa is trying to deliver presents in a large apartment building, but he can't find the right
+//! floor - the directions he got are a little confusing. He starts on the ground floor (floor 0)
+//! and then follows the instructions one character at a time.
+//!
+//! An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means
+//! he should go down one floor.
+//!
+//! The apartment building is very tall, and the basement is very deep; he will never find the top
+//! or bottom floors.
+//!
+//! For example:
+//!
+//!    (()) and ()() both result in floor 0.
+//!    ((( and (()(()( both result in floor 3.
+//!    ))((((( also results in floor 3.
+//!    ()) and ))( both result in floor -1 (the first basement level).
+//!    ))) and )())()) both result in floor -3.
+//!
+//! To what floor do the instructions take Santa?
+//!
+//! An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
+//!
+//! The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
+//!
+//! ## Solution
+//!
+//!
 use std::fs;
 
-fn main() {
+/// This is main()
+pub fn main() {
     let s = fs::read_to_string("input.txt")
         .expect("Unable to read file")
         .replace("\n", "");
