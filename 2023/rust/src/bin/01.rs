58fa5f06a4a003af612cfcbb2aa8a754669e9a46
noprobelm <noprobelm@protonmail.com>
2023-12-03 19:24:12 -0500

    Removed regex, revised day 1 pt 2 sol
    Day 1 pt 2 still in progress

--- 2023/rust/src/bin/01.rs
+++ 2023/rust/src/bin/01.rs
@@ -1,5 +1,4 @@
 use aoc::PuzzleInput;
-use regex::Regex;
 use std::collections::HashMap;
 
 fn main() {
@@ -10,65 +9,71 @@ fn main() {
     println!("Part 1: {part_1}");
 
     let part_2 = part_2(&lines);
-    println!("Part 2: TODO: Solve overlapping patterns at the end of a string, like 'twone', which our regex will only see as 'two'");
+    println!("Part 2: {part_2}");
 }
 
 fn part_1(lines: &Vec<&str>) -> u32 {
     let mut answer = 0;
     for line in lines {
-        let nums: Vec<char> = line.chars().filter(|c| c.is_digit(10)).collect();
-        let first = nums.first().unwrap();
-        let last = nums.last().unwrap();
-        answer += format!("{first}{last}")
-            .parse::<u32>()
-            .expect("Invalid u32");
+        let digits: Vec<char> = line.chars().filter(|c| c.is_digit(10)).collect();
+        let first = digits.first().unwrap();
+        let last = digits.last().unwrap();
+        let digit = format!("{}{}", first, last);
+
+        answer += digit.parse::<u32>().unwrap();
     }
     answer
 }
 
 fn part_2(lines: &Vec<&str>) -> u32 {
     let mut answer: u32 = 0;
-    let mapper: HashMap<&str, &str> = HashMap::from([
-        ("one", "1"),
-        ("two", "2"),
-        ("three", "3"),
-        ("four", "4"),
-        ("five", "5"),
-        ("six", "6"),
-        ("seven", "7"),
-        ("eight", "8"),
-        ("nine", "9"),
-        ("oneeight", "18"),
-        ("twoone", "21"),
-        ("threeeight", "38"),
-        ("sevennine", "79"),
+    let patterns: [&str; 18] = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4",
+        "5", "6", "7", "8", "9",
+    ];
+    let mapper: HashMap<&str, u32> = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
     ]);
 
-    let mut pattern = mapper
-        .keys()
-        .map(|k| format!("({k})"))
-        .collect::<Vec<String>>();
-    for i in 1..=9 {
-        pattern.push(format!("{i}"));
-    }
-    let re = Regex::new(pattern.join("|").as_str()).unwrap();
     for line in lines {
-        let matches: Vec<&str> = re
-            .find_iter(line)
-            .map(|m| {
-                if let Some(v) = mapper.get(m.as_str()) {
-                    return *v;
+        let mut start: usize = 0;
+        let mut end: usize = 0;
+        let mut matches: Vec<u32> = Vec::new();
+        while start < line.len() {
+            if patterns.contains(&&line[start..=end]) {
+                let matched = &line[start..=end];
+                // We matched a word
+                if let Some(val) = mapper.get(matched) {
+                    matches.push(*val);
+                    start = end;
+                // We matched a digit
                 } else {
-                    return m.as_str();
+                    matches.push(matched.parse::<u32>().expect("Invalid u32"));
+                    start += 1;
+                    end = start;
                 }
-            })
-            .collect();
-        let nums = format!("{}{}", matches.first().unwrap(), matches.last().unwrap());
-        let n = nums
-            .chars()
-            .fold(0, |acc, c| acc * 10 + c.to_digit(10).unwrap());
+            } else {
+                end += 1;
+            }
+            if end == line.len() {
+                start += 1;
+                end = start;
+            }
+        }
+        let n = [*matches.first().unwrap(), *matches.last().unwrap()]
+            .iter()
+            .fold(0, |acc, x| acc * 10 + x);
+        println!("{line}: {:?}, {n}", matches);
 
-        answer += n
+        answer += n;
     }
     answer
 }
