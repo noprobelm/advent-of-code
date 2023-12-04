ea5f5c6f51f94ab84b7ea7dbebeb9eb9fd0fc02b
noprobelm <noprobelm@protonmail.com>
2023-12-03 21:24:25 -0500

    Renamed patterns to pattern, added docs

--- 2023/rust/src/bin/01.rs
+++ 2023/rust/src/bin/01.rs
@@ -1,3 +1,58 @@
+//! # Advent of Code - 2023 Day 2: Cube Conundrum
+//!
+//! ## Problem Description: Part 1
+//! Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given
+//! you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
+//!
+//! You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by
+//! December 25th.
+//!
+//! Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second
+//! puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+//!
+//! You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending you
+//! ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the
+//! sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a
+//! trebuchet ("please hold still, we need to strap you in").
+//!
+//! As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been
+//! amended by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are
+//! having trouble reading the values on the document.
+//!
+//! The newly-improved calibration document consists of lines of text; each line originally contained a specific
+//! calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining
+//! the first digit and the last digit (in that order) to form a single two-digit number.
+//!
+//! For example:
+//! - 1abc2
+//! - pqr3stu8vwx
+//! - a1b2c3d4e5f
+//! - treb7uchet
+//!
+//! In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces
+//! 142.
+//!
+//! Consider your entire calibration document. What is the sum of all of the calibration values?
+//!
+//! ## Problem Description: Part 2
+//!
+//! Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one,
+//! two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+//!
+//! Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+//!
+//! - two1nine
+//! - eightwothree
+//! - abcone2threexyz
+//! - xtwone3four
+//! - 4nineeightseven2
+//! - zoneight234
+//! - 7pqrstsixteen
+//!
+//! In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+//!
+//! What is the sum of all of the calibration values?
+
 use aoc::PuzzleInput;
 use std::collections::HashMap;
 
@@ -27,11 +82,13 @@ fn part_1(lines: &Vec<&str>) -> u32 {
 
 fn part_2(lines: &Vec<&str>) -> u32 {
     let mut answer: u32 = 0;
-    let patterns: [&str; 18] = [
+    let pattern: [&str; 18] = [
         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4",
         "5", "6", "7", "8", "9",
     ];
-    // The max characlength of any of our patterns
+
+    // The max character length found among our patterns
+
     let max_match_len = 5;
 
     let mapper: HashMap<&str, u32> = HashMap::from([
@@ -51,7 +108,7 @@ fn part_2(lines: &Vec<&str>) -> u32 {
         let mut end: usize = 0;
         let mut matches: Vec<u32> = Vec::new();
         while start < line.len() {
-            if patterns.contains(&&line[start..=end]) {
+            if pattern.contains(&&line[start..=end]) {
                 let matched = &line[start..=end];
                 // We matched a word
                 if let Some(val) = mapper.get(matched) {
