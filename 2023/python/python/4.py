3ae6408ff7f949b7a7849e865100d90aa85eb362
noprobelm <noprobelm@protonmail.com>
2023-12-05 15:15:22 -0500

    Added commentary for part 2

--- 2023/python/python/4.py
+++ 2023/python/python/4.py
@@ -40,6 +40,7 @@ def part_2(lines: list[str]):
 
     for card_id in cards:
         matches = winnings[card_id]
+        # Could use reduce() here, but I like to stick to the stdlib for these puzzles.
         for n in range(len(matches)):
             cards.append(card_id + n + 1)
 
