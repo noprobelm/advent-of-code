d7960a0a67d722524bbd88608220da3e318c9d41
noprobelm <noprobelm@protonmail.com>
2024-12-01 01:32:08 -0500

    Solved day 1 part 1 2024 in rust

---
+++ 2024/rust/src/bin/01.rs
@@ -0,0 +1,40 @@
+use aoc::PuzzleInput;
+use std::collections::HashMap;
+
+fn main() {
+    let p = PuzzleInput::new("../data/1.txt");
+    let lines = p.lines();
+
+    let part_1 = part_1(&lines);
+    println!("Part 1: {part_1}");
+}
+
+fn part_1(lines: &Vec<&str>) -> u32 {
+    let mut distance: i32 = 0;
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+    for line in lines {
+        line.split("   ")
+            .map(|n| n.parse::<i32>().expect("Not a valid u32!"))
+            .collect::<Vec<i32>>()
+            .iter()
+            .enumerate()
+            .for_each(|(i, n)| {
+                if i % 2 == 0 {
+                    right.push(*n)
+                } else {
+                    left.push(*n)
+                }
+            });
+    }
+
+    left.sort();
+    right.sort();
+    let zipped: Vec<(&i32, &i32)> = left.iter().zip(right.iter()).collect();
+    zipped.iter().for_each(|(left_n, right_n)| {
+        distance += (**left_n - **right_n).abs();
+    });
+
+    distance.try_into().unwrap()
+}
+
