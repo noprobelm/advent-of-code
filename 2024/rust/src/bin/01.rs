c3dd4cfd722eac053fe805b79e44d0d65b32df9a
noprobelm <noprobelm@protonmail.com>
2024-12-01 13:02:20 -0500

    Revised part 2

--- 2024/rust/src/bin/01.rs
+++ 2024/rust/src/bin/01.rs
@@ -4,50 +4,9 @@ fn main() {
     let p = PuzzleInput::new("../data/1.txt");
     let lines = p.lines();
 
-    let part_1 = part_1(&lines);
-    println!("Part 1: {part_1}");
-
-    let part_2 = part_2(&lines);
-    println!("Part 2: {part_2}");
-}
-
-fn part_1(lines: &Vec<&str>) -> u32 {
-    let mut left: Vec<i32> = Vec::new();
-    let mut right: Vec<i32> = Vec::new();
-    lines.iter().for_each(|line| {
-        line.split("   ")
-            .map(|n| n.parse::<i32>().expect("Not a valid i32!"))
-            .collect::<Vec<i32>>()
-            .iter()
-            .enumerate()
-            .for_each(|(i, n)| {
-                if i % 2 == 0 {
-                    right.push(*n)
-                } else {
-                    left.push(*n)
-                }
-            });
-    });
-
-    left.sort();
-    right.sort();
-
-    left.iter()
-        .zip(right.iter())
-        .collect::<Vec<(&i32, &i32)>>()
-        .iter()
-        .fold(0, |acc, (left_n, right_n)| {
-            acc + (**left_n - **right_n).abs()
-        })
-        .try_into()
-        .unwrap()
-}
-
-fn part_2(lines: &Vec<&str>) -> u32 {
-    let mut count: u32 = 0;
     let mut left: Vec<u32> = Vec::new();
     let mut right: Vec<u32> = Vec::new();
-    for line in lines {
+    lines.iter().for_each(|line| {
         line.split("   ")
             .map(|n| n.parse::<u32>().expect("Not a valid u32!"))
             .collect::<Vec<u32>>()
@@ -60,16 +19,42 @@ fn part_2(lines: &Vec<&str>) -> u32 {
                     left.push(*n)
                 }
             });
-    }
-
-    left.iter().for_each(|left_n| {
-        let len: u32 = right
-            .iter()
-            .filter(|right_n| *right_n == left_n)
-            .collect::<Vec<&u32>>()
-            .len() as u32;
-        count += left_n * len;
     });
 
-    count
+    let part_1 = part_1(&left, &right);
+    println!("Part 1: {part_1}");
+
+    let part_2 = part_2(&left, &right);
+    println!("Part 2: {part_2}");
+}
+
+fn part_1(left: &Vec<u32>, right: &Vec<u32>) -> u32 {
+    let mut left = left.clone();
+    let mut right = right.clone();
+
+    left.sort();
+    right.sort();
+
+    left.iter()
+        .zip(right.iter())
+        .collect::<Vec<(&u32, &u32)>>()
+        .iter()
+        .fold(0, |acc, (left_n, right_n)| {
+            if left_n > right_n {
+                return acc + **left_n - **right_n;
+            } else {
+                return acc + **right_n - **left_n;
+            }
+        })
+}
+
+fn part_2(left: &Vec<u32>, right: &Vec<u32>) -> u32 {
+    left.iter().fold(0, |acc, left_n| {
+        acc + left_n
+            * (right
+                .iter()
+                .filter(|right_n| *right_n == left_n)
+                .collect::<Vec<&u32>>()
+                .len() as u32)
+    })
 }
