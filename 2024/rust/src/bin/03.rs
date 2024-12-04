e4ccfa02cec2173708a6a292b9251058db12c3c0
noprobelm <noprobelm@protonmail.com>
2024-12-03 23:15:06 -0500

    Solved day 3 part 2, but I'd like to do all of part 2 using regex

--- 2024/rust/src/bin/03.rs
+++ 2024/rust/src/bin/03.rs
@@ -8,14 +8,15 @@ fn main() {
     let part_1 = part_1(s);
     println!("Part 1: {part_1}");
 
-    // let part_2 = part_2(&lines);
-    // println!("Part 2: {part_2}");
+    let part_2 = part_2(s);
+    println!("Part 2: {part_2}");
 }
 
 fn part_1(puzzle_input_str: &str) -> u32 {
     let re = Regex::new(r#"mul\((\d+,\d+)\)"#).unwrap();
     re.captures_iter(puzzle_input_str).fold(0, |acc, m| {
-        acc + m.get(1)
+        acc + m
+            .get(1)
             .unwrap()
             .as_str()
             .split(",")
@@ -26,7 +27,23 @@ fn part_1(puzzle_input_str: &str) -> u32 {
     })
 }
 
-fn part_2(lines: &Vec<&str>) -> u32 {
+fn part_2(puzzle_input_str: &str) -> u32 {
     let mut answer = 0;
+    let re = Regex::new(r#"mul\((\d+,\d+)\)"#).unwrap();
+    let split: Vec<&str> = puzzle_input_str.split("do()").collect();
+    for item in split {
+        let new: &str = item.split("don't()").collect::<Vec<&str>>().get(0).unwrap();
+        answer += re.captures_iter(new).fold(0, |acc, m| {
+            acc + m
+                .get(1)
+                .unwrap()
+                .as_str()
+                .split(",")
+                .map(|n| n.parse::<u32>().expect("Invalid u32!"))
+                .collect::<Vec<u32>>()
+                .iter()
+                .fold(1, |acc, n| acc * n)
+        });
+    }
     answer
 }
