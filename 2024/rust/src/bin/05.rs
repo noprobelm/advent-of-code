9e3061c0f79f34a733ad1843e10493fa72117b16
noprobelm <noprobelm@protonmail.com>
2024-12-08 11:13:12 -0500

    Organized day 5 solutions

--- 2024/rust/src/bin/05.rs
+++ 2024/rust/src/bin/05.rs
@@ -3,20 +3,21 @@ use std::collections::HashMap;
 
 fn main() {
     let p = PuzzleInput::new("../data/5.txt");
-    let lines = p.lines();
 
-    let part_1 = part_1(&lines);
+    let (map, page_updates) = parse_puzzle_input(p);
+
+    let part_1 = part_1(map.clone(), page_updates.clone());
     println!("Part 1: {part_1}");
 
-    let part_2 = part_2(&lines);
+    let part_2 = part_2(map, page_updates);
     println!("Part 2: {part_2}");
 }
 
-fn part_1(lines: &Vec<&str>) -> u32 {
-    let mut answer = 0;
+fn parse_puzzle_input(puzzle_input: PuzzleInput) -> (HashMap<u32, Vec<u32>>, Vec<Vec<u32>>) {
     let mut map: HashMap<u32, Vec<u32>> = HashMap::new();
     let mut page_updates: Vec<Vec<u32>> = Vec::new();
 
+    let lines = puzzle_input.lines();
     let (first_section, second_section) =
         lines.split_at(lines.iter().position(|line| *line == "").unwrap());
 
@@ -45,60 +46,7 @@ fn part_1(lines: &Vec<&str>) -> u32 {
         page_updates.push(updates)
     });
 
-    page_updates.iter().for_each(|page| {
-        if ordered(&map, page).is_ok() {
-            answer += page.get(page.len() / 2).unwrap();
-        }
-    });
-
-    answer
-}
-
-fn part_2(lines: &Vec<&str>) -> u32 {
-    let mut answer = 0;
-    let mut map: HashMap<u32, Vec<u32>> = HashMap::new();
-    let mut page_updates: Vec<Vec<u32>> = Vec::new();
-
-    let (first_section, second_section) =
-        lines.split_at(lines.iter().position(|line| *line == "").unwrap());
-
-    first_section.iter().for_each(|page_numbers_str| {
-        let page_numbers: Vec<u32> = page_numbers_str
-            .split("|")
-            .map(|s| {
-                s.parse::<u32>()
-                    .expect("Found invalid u32 while parsing rules.")
-            })
-            .collect();
-
-        map.entry(page_numbers[0])
-            .and_modify(|v| v.push(page_numbers[1]))
-            .or_insert(vec![page_numbers[1]]);
-    });
-
-    second_section.iter().skip(1).for_each(|updates_str| {
-        let updates: Vec<u32> = updates_str
-            .split(",")
-            .map(|s| {
-                s.parse::<u32>()
-                    .expect("Found invalid u32 while parsing page updates.")
-            })
-            .collect();
-        page_updates.push(updates)
-    });
-
-    page_updates.iter_mut().for_each(|mut page| {
-        let mut unsorted: bool = false;
-        while let Err(failed_index) = ordered(&map, page) {
-            unsorted = true;
-            page.swap(failed_index, failed_index - 1);
-        }
-        if unsorted {
-            answer += page.get(page.len() / 2).unwrap();
-        }
-    });
-
-    answer
+    (map, page_updates)
 }
 
 fn ordered(map: &HashMap<u32, Vec<u32>>, page: &Vec<u32>) -> Result<(), usize> {
@@ -112,4 +60,27 @@ fn ordered(map: &HashMap<u32, Vec<u32>>, page: &Vec<u32>) -> Result<(), usize> {
         explored.push(*page);
     }
     Ok(())
+}
+
+fn part_1(map: HashMap<u32, Vec<u32>>, page_updates: Vec<Vec<u32>>) -> u32 {
+    page_updates.iter().fold(0, |acc, page| {
+        if ordered(&map, page).is_ok() {
+            return acc + page.get(page.len() / 2).unwrap();
+        }
+        acc
+    })
+}
+
+fn part_2(map: HashMap<u32, Vec<u32>>, mut page_updates: Vec<Vec<u32>>) -> u32 {
+    page_updates.iter_mut().fold(0, |acc, page| {
+        let mut unsorted: bool = false;
+        while let Err(failed_index) = ordered(&map, page) {
+            unsorted = true;
+            page.swap(failed_index, failed_index - 1);
+        }
+        if unsorted {
+            return acc + page.get(page.len() / 2).unwrap();
+        }
+        acc
+    })
 }
