170502d9511aa244a658470f2ea73f8bab541934
noprobelm <noprobelm@protonmail.com>
2025-05-24 15:06:46 -0400

    Correct answer... But why

--- 2024/rust/src/bin/08.rs
+++ 2024/rust/src/bin/08.rs
@@ -10,7 +10,7 @@ fn main() {
     println!("Part 2: {}", part_2);
 }
 
-fn part_1(puzzle_input: &PuzzleInput) -> u32 {
+fn part_1(puzzle_input: &PuzzleInput) -> usize {
     let matrix = Matrix::from_chars(puzzle_input.lines());
     let mut antinodes: HashSet<Position> = HashSet::new();
     matrix.iter().for_each(|antenna| {
@@ -20,27 +20,65 @@ fn part_1(puzzle_input: &PuzzleInput) -> u32 {
                 && antenna.position != other.position
             {
                 let diff = antenna.position - other.position;
+                println!("{:?}", diff);
                 let first_antinode = antenna.position
                     - Position {
                         x: diff.x * 2,
                         y: diff.y * 2,
                     };
+                println!("First antinode: {:?}", first_antinode);
                 if matrix.contains(first_antinode) {
                     antinodes.insert(first_antinode);
                 }
-                let second_antinode = other.position - diff;
+                let second_antinode = antenna.position + diff;
+                println!("Second antinode: {:?}", second_antinode);
                 if matrix.contains(second_antinode) {
                     antinodes.insert(second_antinode);
                 }
             }
         });
     });
-    println!("{:?}", antinodes.len());
-    0
+    antinodes.len()
 }
 
-fn part_2(puzzle_input: &PuzzleInput) -> u32 {
-    0
+fn part_2(puzzle_input: &PuzzleInput) -> usize {
+    let matrix = Matrix::from_chars(puzzle_input.lines());
+    let mut antinodes: HashSet<Position> = HashSet::new();
+    matrix.iter().for_each(|antenna| {
+        matrix.iter().for_each(|other| {
+            if antenna.variant != '.'
+                && antenna.variant == other.variant
+                && antenna.position != other.position
+            {
+                let diff = antenna.position - other.position;
+                let mut first_antinode = antenna.position
+                    - Position {
+                        x: diff.x * 2,
+                        y: diff.y * 2,
+                    };
+                while matrix.contains(first_antinode) {
+                    antinodes.insert(first_antinode);
+                    first_antinode -= diff;
+                }
+                let mut second_antinode = antenna.position - diff;
+                while matrix.contains(second_antinode) {
+                    antinodes.insert(second_antinode);
+                    second_antinode += diff;
+                }
+            }
+        });
+    });
+    // let mut new_matrix = matrix.clone();
+    // matrix.iter().for_each(|antenna| {
+    //     if antenna.variant == '.' && antinodes.contains(&antenna.position) {
+    //         new_matrix.insert(Antenna::new(antenna.position, '#'));
+    //     } else {
+    //         new_matrix.insert(*antenna);
+    //     }
+    // });
+    // let s = new_matrix.strf();
+    // println!("{s}");
+    antinodes.len()
 }
 
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
@@ -85,6 +123,31 @@ impl Matrix {
     fn contains(&self, pos: Position) -> bool {
         pos.x < self.xmax && pos.y < self.ymax && pos.x >= 0 && pos.y >= 0
     }
+
+    fn get(&self, pos: Position) -> Option<&Antenna> {
+        if self.contains(pos) {
+            Some(&self.matrix[pos.y as usize][pos.x as usize])
+        } else {
+            None
+        }
+    }
+
+    fn insert(&mut self, antenna: Antenna) {
+        if self.contains(antenna.position) {
+            self.matrix[antenna.position.y as usize][antenna.position.x as usize] = antenna;
+        }
+    }
+
+    fn strf(&self) -> String {
+        let mut s = String::new();
+        for row in &self.matrix {
+            for antenna in row {
+                s.push(antenna.variant);
+            }
+            s.push('\n');
+        }
+        s
+    }
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
@@ -104,6 +167,13 @@ impl std::ops::Add for Position {
     }
 }
 
+impl std::ops::AddAssign for Position {
+    fn add_assign(&mut self, other: Position) {
+        self.x += other.x;
+        self.y += other.y;
+    }
+}
+
 impl std::ops::Sub for Position {
     type Output = Position;
 
@@ -112,6 +182,31 @@ impl std::ops::Sub for Position {
             x: self.x - other.x,
             y: self.y - other.y,
         }
+    }
+}
+
+impl std::ops::SubAssign for Position {
+    fn sub_assign(&mut self, other: Position) {
+        self.x -= other.x;
+        self.y -= other.y;
+    }
+}
+
+impl std::ops::Mul<i32> for Position {
+    type Output = Position;
+
+    fn mul(self, other: i32) -> Position {
+        Position {
+            x: self.x * other,
+            y: self.y * other,
+        }
+    }
+}
+
+impl std::ops::MulAssign<i32> for Position {
+    fn mul_assign(&mut self, other: i32) {
+        self.x *= other;
+        self.y *= other;
     }
 }
 
