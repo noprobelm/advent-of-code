44b2fd48500feea979477fa7d17cf6d48f3baaec
Jeff Barfield <jeffreybarfield@protonmail.com>
2025-12-13 22:30:04 -0500

    Solve day 3 parts 1 and 2

--- 2025/python/python/3.py
+++ 2025/python/python/3.py
@@ -1,3 +1,5 @@
+from rich import print
+
 puzzle_input = [
     "987654321111111",
     "811111111111119",
@@ -5,27 +7,42 @@ puzzle_input = [
     "818181911112111",
 ]
 
-# with open("../../data/3.txt", "r") as f:
-#     puzzle_input = f.readlines()
+with open("../../data/3.txt", "r") as f:
+    puzzle_input = [line.strip() for line in f.readlines()]
 
 
 def part_1():
-    jolts = []
+    joltages = []
     for battery in puzzle_input:
         battery = list(battery.strip())
         largest = max(battery[: len(battery) - 1])
         next_largest = max(battery[battery.index(largest) + 1 :])
-        jolts.append(int("".join([largest, next_largest])))
+        joltages.append(int("".join([largest, next_largest])))
 
-    return sum(jolts)
+    return sum(joltages)
 
 
 def part_2():
-    pass
+    joltages = []
+    for battery in puzzle_input:
+        joltage = []
+        i = 0
+        skips_remaining = len(battery) - 12
+        while i < len(battery) and len(joltage) < 12:
+            local_max_idx = i
+            for k in range(local_max_idx, local_max_idx + skips_remaining + 1):
+                if k < len(battery) and battery[k] > battery[local_max_idx]:
+                    local_max_idx = k
+            skips_remaining -= local_max_idx - i
+            joltage.append(battery[local_max_idx])
+            i = local_max_idx + 1
+        joltages.append(int("".join(joltage)))
+
+    return sum(joltages)
 
 
 if __name__ == "__main__":
     answer = part_1()
     print(answer)
-    # answer = part_2()
-    # print(answer)
+    answer = part_2()
+    print(answer)
